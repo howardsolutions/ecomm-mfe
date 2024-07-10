d664382c35bbe38498ccb815ba9f76502212e095
thinhphungdev <phungthinh0946519356@gmail.com>
2024-07-10 16:08:36 +0700

    refactor: product

--- products/src/boostrap.js
+++ products/src/boostrap.js
@@ -1,10 +1,27 @@
 import faker from 'faker';
 
-let products = '';
 
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
- }
+function productMount(el) {
+    let products = '';
 
-document.querySelector("#dev-products").innerHTML = products;
+    for (let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`
+    };
+
+    document.querySelector(el).innerHTML = products;
+}
+
+// Context 1: Running the product in isolation in development
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-products');
+
+    // ASUMMING our container doesnt have an element with id 'dev-products'
+    if (el) {
+        productMount(el);
+    }
+}
+
+// Context 2: Running this file in Development or production through CONTAINER (HOST) app
+
+export {productMount}
