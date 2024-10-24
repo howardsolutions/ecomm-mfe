296be615b4ccadcfeabcd19868ffdb76435d1067
thinhphungdev <phungthinh0946519356@gmail.com>
2024-10-24 11:23:02 +0700

    chore: trigger pipeline manually

--- mfp/container/src/bootstrap.js
+++ mfp/container/src/bootstrap.js
@@ -5,3 +5,4 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 console.log("This log for testing the pipeline on container")
+console.log("Manually triggering the pipeline to run")
