e1f17d2b4db5fe9405c98d93c4e1b6b1ea405fe5
thinhphungdev <phungthinh0946519356@gmail.com>
2024-10-25 10:50:10 +0700

    fix: small import issue

--- mfp/container/src/App.js
+++ mfp/container/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'cont',
